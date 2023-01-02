6e533aeb5ecec9cb0970ffa55e7da705aa834f4b
Carlos Hernández <Calichi15@hotmail.com>
2023-01-02 13:27:34 -0600

    Grafico de barras

--- src/modules/canvatools.mjs
+++ src/modules/canvatools.mjs
@@ -12,4 +12,38 @@ function drawCircle(x, y, r, fill, canvas) {
   canvas.fill();
 }
 
-export { getCanvasById, drawCircle };
+function fillRectangle(x, y, w, h, fill, canvas) {
+  canvas.fillStyle = fill;
+  canvas.fillRect(x, y, w, h);
+}
+
+function drawRectangle(x, y, w, h, stroke, canvas) {
+  canvas.strokeStyle = stroke;
+  canvas.strokeRect(x, y, w, h);
+}
+
+function fillText(x, y, text, font, fill, canvas) {
+  canvas.font = font;
+  canvas.fillStyle = fill;
+  canvas.fillText(text, x, y);
+}
+
+function measureText(text, font, canvas) {
+  canvas.font = font;
+  return canvas.measureText(text);
+}
+
+function drawConnection(x1, x2, x3, y1, y2, stroke, canvas) {
+  canvas.strokeStyle = stroke;
+  canvas.beginPath();
+  canvas.moveTo(x1, y2);
+  canvas.lineTo(x2, y2);
+  canvas.lineTo(x2, y1);
+  canvas.lineTo(x3, y1);
+  canvas.stroke();
+}
+
+export { getCanvasById, drawCircle, drawRectangle,
+         fillRectangle,
+         fillText, measureText,
+         drawConnection };
