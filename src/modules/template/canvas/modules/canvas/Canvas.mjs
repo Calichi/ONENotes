b365b8fb2ba26d8cbaed8da745e4aca1048fd7a1
Carlos Hernández <Calichi15@hotmail.com>
2023-01-04 23:42:13 -0600

    Creando sistema canvas

---
+++ src/modules/template/canvas/modules/canvas/Canvas.mjs
@@ -0,0 +1,4 @@
+export { Size } from './Size.mjs';
+export { Point } from './Point.mjs';
+export { Rect } from './Rect.mjs';
+export { Drawing } from './Drawing.mjs';
---
+++ src/modules/template/canvas/modules/canvas/Drawing.mjs
@@ -0,0 +1,39 @@
+'use strict';
+
+import { Size } from './Size.mjs';
+//import { Point } from './Point.mjs'
+//import { Rect } from './Rect.mjs'
+
+exports = class Drawing {
+  //Miembros estaticos
+  static #_HTML_CANVAS;
+  static #_CANVAS;
+  static #_SCREEN_SIZE;
+
+  static get CANVAS() { return this.#_CANVAS; }
+  static get HTML_CANVAS() { return this.#_HTML_CANVAS; }
+  static get SCREEN_SIZE() { return this.#_SCREEN_SIZE; }
+  static set HTML_CANVAS(value) {
+    this.#_HTML_CANVAS = value;
+    this.#_SCREEN_SIZE = new Size(value.clientWidth, value.clientHeight);
+    this.#_CANVAS = value.getContext('2d');
+  }
+
+  static LoadCanvas(id) {
+    this.HTML_CANVAS = document.getElementById(id);
+  }
+  //Miembros de instancia
+  #_region;
+  #_pigment;
+
+  constructor(region, pigment) {
+    this.#_region = region;
+    this.#_pigment = pigment;
+  }
+
+  get pigment() { return this.#_pigment; }
+  set pigment(value) { this.#_pigment = value; }
+  get region() { return this.#_region; }
+  set region(value) {this.#_region = value;}
+
+}
