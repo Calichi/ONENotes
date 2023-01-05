3545153ef14686c2cb58c7912bf25af0de80a984
Carlos Hernández <Calichi15@hotmail.com>
2023-01-04 23:45:47 -0600

    Probando sistema canvas

---
+++ src/practice/example/modules/canvatools/canvatools.mjs
@@ -0,0 +1,49 @@
+"use strict";
+
+function getCanvasById(id) {
+  let htmlElement = document.getElementById(id);
+  return htmlElement.getContext('2d');
+}
+
+function paintCircle(x, y, r, fill, canvas) {
+  canvas.fillStyle = fill;
+  canvas.beginPath();
+  canvas.arc(x, y, r, 0, 2 * Math.PI);
+  canvas.fill();
+}
+
+function paintRectangle(x, y, w, h, fill, canvas) {
+  canvas.fillStyle = fill;
+  canvas.fillRect(x, y, w, h);
+}
+
+function strokeRectangle(x, y, w, h, stroke, canvas) {
+  canvas.strokeStyle = stroke;
+  canvas.strokeRect(x, y, w, h);
+}
+
+function fillText(x, y, text, font, fill, canvas) {
+  canvas.font = font;
+  canvas.fillStyle = fill;
+  canvas.fillText(text, x, y);
+}
+
+function measureText(text, font, canvas) {
+  canvas.font = font;
+  return canvas.measureText(text);
+}
+
+function drawConnection(x1, x2, x3, y1, y2, stroke, canvas) {
+  canvas.strokeStyle = stroke;
+  canvas.beginPath();
+  canvas.moveTo(x1, y2);
+  canvas.lineTo(x2, y2);
+  canvas.lineTo(x2, y1);
+  canvas.lineTo(x3, y1);
+  canvas.stroke();
+}
+
+export { getCanvasById, paintCircle, strokeRectangle,
+         paintRectangle,
+         fillText, measureText,
+         drawConnection };
---
+++ src/practice/example/src/code.js
@@ -0,0 +1,15 @@
+"use strict";
+
+import { paintRectangle } from '../modules/canvatools/canvatools.mjs';
+import { Size, Point, Rect, Drawing } from '../modules/canvas/Canvas.mjs';
+
+const htmlCanvas = document.getElementById('canvas');
+const canvas = htmlCanvas.getContext('2d');
+
+const h = htmlCanvas.clientHeight;
+const w = htmlCanvas.clientWidth;
+const mbg = '#024';
+
+paintRectangle(0, 0, w, h, mbg, canvas);
+
+// Código del programa
