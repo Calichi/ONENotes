3d120e68594b4d29e3c5b4379ec72ce295044c9f
Carlos Hernández <Calichi15@hotmail.com>
2023-02-08 13:45:57 -0600

    Tipo entero

--- Java OOP/JRE-and-JDK/first_steps/src/first_steps/example.java
+++ Java OOP/JRE-and-JDK/first_steps/src/first_steps/example.java
@@ -1,11 +1,18 @@
 package first_steps;
 
+//Tipos de variable
+
 public class example {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Hola mundo");
 		
+		int age = 26;
+		
+		System.out.println(age);
+		
+		System.out.printf("Mi edad es %1s", age);
 	}
 	
 }
---
+++ Java OOP/JRE-and-JDK/syntax_variable_and_flow/src/TestVariables.java
@@ -0,0 +1,12 @@
+public class TestVariables {
+  
+  public static void main(String[] args) {
+
+    int age = 26;
+
+    System.out.println("¡Hola nuevo test!");
+    System.out.printf("Mi edad es %1s años", age);
+
+  }
+
+}
