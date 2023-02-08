1c321ec29ba54bb1d7fd6df4161a3954999c43ca
Carlos Hernández <Calichi15@hotmail.com>
2023-02-08 14:25:08 -0600

    Tipo double

--- Java OOP/JRE-and-JDK/syntax_variable_and_flow/src/TestVariables.java
+++ Java OOP/JRE-and-JDK/syntax_variable_and_flow/src/TestVariables.java
@@ -2,10 +2,15 @@ public class TestVariables {
   
   public static void main(String[] args) {
 
-    int age = 26;
+    double salary = 1849.50;
+    double halfSalary = salary / 2;
+    int division = 1849 / 3;
 
-    System.out.println("¡Hola nuevo test!");
-    System.out.printf("Mi edad es %1s años", age);
+    System.out.printf("Su salario es: %1s", salary);
+    System.out.println();
+    System.out.printf("Su salario dividido a la mitad es: %1s", halfSalary);
+    System.out.println();
+    System.out.printf("Dividiendo un int: %1s", division);
 
   }
 
