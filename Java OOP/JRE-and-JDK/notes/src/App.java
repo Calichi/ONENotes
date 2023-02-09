1d50be5ba5d66274323053d7d96826ff95d3109d
Carlos Hernández <Calichi15@hotmail.com>
2023-02-09 10:26:40 -0600

    Conversiones

--- Java OOP/JRE-and-JDK/notes/src/App.java
+++ Java OOP/JRE-and-JDK/notes/src/App.java
@@ -1,5 +1,27 @@
 public class App {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        double __double = 230.89;
+        int __integer = (int)__double; //Cast
+        long __long = 12L;
+        short __short = 13555;
+        byte __byte = 127;
+        float __float = 2.5F;
+
+        //Este método imprime en consola
+        print(__double);
+        print(__integer);
+        print(__long);
+        print(__short);
+        print(__byte);
+        print(__float);
+
     }
+
+    public static void print(Object obj) {
+        System.out.printf("%1s", obj);
+        System.out.println();
+    }
+
 }
