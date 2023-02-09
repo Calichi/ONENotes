a89e9b07317a71aa2cb0fa9f3a380683fb083385
Carlos Hernández <Calichi15@hotmail.com>
2023-02-09 10:42:56 -0600

    Algunas conversion en Java

---
+++ Java OOP/JRE-and-JDK/practice/Conversion/src/Convertion.java
@@ -0,0 +1,20 @@
+public class Convertion {
+
+    public static void main(String[] args) throws Exception {
+
+        float _float = 3.14f;
+        
+        double salary = 1270.50;
+        int value = (int) salary;
+
+        System.out.println(value);
+
+        double value1 = 0.2;
+        double value2 = 0.1;
+        double total = value1 + value2;
+
+        System.out.println(total);
+
+    }
+
+}
