c11bfdaa4b0028e4eff302e71441a30c3810d5b1
Carlos Hernández <Calichi15@hotmail.com>
2023-02-14 10:33:12 -0600

    Ciclos anidados

--- Java OOP/JRE-and-JDK/notes/src/App.java
+++ Java OOP/JRE-and-JDK/notes/src/App.java
@@ -2,26 +2,23 @@ public class App {
 
     public static void main(String[] args) throws Exception {
 
-        //int count = 0;
-        int sum = 0;
-
-        /*while(count < 10) {
-            count++;
-            sum += count;
-            print(count);
-        }*/
-
-        for(int count = 0; count <= 10; ++count) {
-            sum += count;
-            print(count);
+        for(int multiplicand = 1; multiplicand <= 10; multiplicand++) {
+            for(int multiplier = 1; multiplier <= 10; multiplier++) {
+                print(multiplicand, multiplier);
+            }
+            System.out.println();
         }
-
-        print(sum);
 
     }
 
     public static void print(Object obj) {
         System.out.printf("%1s", obj);
+        System.out.println();
+    }
+
+    public static void print(int multiplicand, int multiplier) {
+        int product = multiplicand * multiplier;
+        System.out.printf("%1s por %2s es igual a %3s", multiplicand, multiplier, product);
         System.out.println();
     }
 
