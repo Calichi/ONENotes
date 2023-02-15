2c2a8218de9a1c562e94f46ffff9b672d8c51510
Carlos Hernández <Calichi15@hotmail.com>
2023-02-15 10:41:30 -0600

    Referencias parte 2

--- Java OOP/Java-OO/bytebank-project/src/Program.java
+++ Java OOP/Java-OO/bytebank-project/src/Program.java
@@ -4,15 +4,20 @@ public class Program {
 
     Account account = new Account();
     Account accountB = account;
+    Account accountC = new Account();
 
     account.balance = 200;
     accountB.balance = 500;
+    accountC.balance = 100;
 
     account.print();
-
     System.out.println(); 
-
     accountB.print();
+    System.out.println(); 
+    accountC.print();
+
+    if(account == accountB) System.out.println("Es la misma cuenta");
+    if(account != accountC) System.out.println("Son cuentas distintas");
 
   }
 
