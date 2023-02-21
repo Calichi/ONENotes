3e0334b5e1ea509f893280f90ee2fad78f8240b3
Carlos Hernández <Calichi15@hotmail.com>
2023-02-21 10:34:14 -0600

    Valores null

--- Java OOP/Java-OO/bytebank-project/src/Program.java
+++ Java OOP/Java-OO/bytebank-project/src/Program.java
@@ -2,15 +2,10 @@ public class Program {
 
   public static void main(String[] args) {
 
-    Client carlos  = new Client();
     Account account = new Account();
 
-    carlos.name = "Carlos";
-    carlos.document = "55555555";
-    carlos.phone = "8991903658";
-
-    account.agency = 1;
-    account.titular = carlos;
+    account.titular = new Client();
+    //account.titular.name = "Carlos";
 
     account.print();
   }
