368343d52ed2ec4e92982b7b9f7ca62cd6d06149
Carlos Hernández <Calichi15@hotmail.com>
2023-02-21 14:53:25 -0600

    Referencias

--- Java OOP/Java-OO/bytebank-project/src/Program.java
+++ Java OOP/Java-OO/bytebank-project/src/Program.java
@@ -3,8 +3,12 @@ public class Program {
   public static void main(String[] args) {
 
     Account account = new Account();
-    account.depositar(100);
-    account.retirar(300);
+    Client client = new Client();
+
+    client.setName("Carlos");
+    client.setDocument("90909090");
+
+    account.setTitular(client);
 
     account.print();
   }
