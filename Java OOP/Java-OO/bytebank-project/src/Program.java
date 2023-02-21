6e01d3f8463b222c54fd5857f544b905b5d205ef
Carlos Hernández <Calichi15@hotmail.com>
2023-02-21 14:18:28 -0600

    Getter

--- Java OOP/Java-OO/bytebank-project/src/Program.java
+++ Java OOP/Java-OO/bytebank-project/src/Program.java
@@ -3,9 +3,8 @@ public class Program {
   public static void main(String[] args) {
 
     Account account = new Account();
-    account.balance = 200;
+    account.depositar(100);
     account.retirar(300);
-    account.balance -= 300;
 
     account.print();
   }
