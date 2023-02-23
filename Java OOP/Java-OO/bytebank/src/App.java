a9056d0a9b99111e147bae4e1139bcaaf81546b7
Carlos Hernández <Calichi15@hotmail.com>
2023-02-23 14:32:01 -0600

    Clase funcionario

--- Java OOP/Java-OO/bytebank/src/App.java
+++ Java OOP/Java-OO/bytebank/src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Funcionario victoria = new Funcionario();
+        victoria.setName("Victoria");
+        victoria.setDocument("88888888");
+        victoria.setSalary(1200);
+
+        System.out.println(victoria);
     }
 }
