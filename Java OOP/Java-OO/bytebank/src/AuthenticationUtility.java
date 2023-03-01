15192e498a42f1bc0a076e57782d32824c7f9e74
Carlos Hernández <Calichi15@hotmail.com>
2023-03-01 15:15:36 -0600

    Usando herencia e interfaces

--- Java OOP/Java-OO/bytebank/src/AuthenticationUtility.java
+++ Java OOP/Java-OO/bytebank/src/AuthenticationUtility.java
@@ -1,3 +1,13 @@
 public class AuthenticationUtility {
+
+  private String key = "12345";
+
+  public void setKey(String key) {
+    this.key = key;
+  }
+
+  public boolean login(String key) {
+    return this.key == key;
+  }
   
 }
