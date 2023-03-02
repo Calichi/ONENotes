9ec07e4a4281d4d233e5587861f2b40009fbbfe2
Carlos Hernández <Calichi15@hotmail.com>
2023-03-01 21:22:47 -0600

    Pila de ejecución

---
+++ Java OOP/Java-Excepciones/java-execution-stack/src/Flow.java
@@ -0,0 +1,20 @@
+public class Flow {
+    public static void main(String[] args) {
+        System.out.println("Inicio de main");
+        method1();
+        System.out.println("Fin de main");
+    }
+
+    private static void method1() {
+        System.out.println("Inicio del método 1");
+        method2();
+        System.out.println("Fin del método 1");
+    }
+
+    private static void method2() {
+        System.out.println("Inicio del método 2");
+        for(int i = 1; i <= 5; i++) System.out.println(i);
+        System.out.println("Fin del método 2");
+    }
+
+}
