eb852f4bd2ba43bb41affc71c130bd9b592101d2
Carlos Hernández <Calichi15@hotmail.com>
2023-03-08 19:16:21 -0600

    Conociendo la interface Set

--- Dominando las collections/alura/src/alura/com/App.java
+++ Dominando las collections/alura/src/alura/com/App.java
@@ -1,70 +1,20 @@
 package alura.com;
 
-import java.util.ArrayList;
-import java.util.stream.Collectors;
-import java.util.List;
-import java.util.Map;
-
-import alura.com.model.Course;
+import java.util.HashSet;
+import java.util.Set;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        ArrayList<Course> courses = new ArrayList<Course>();
-        courses.add(new Course("Historia", 30));
-        courses.add(new Course("Algebra", 10));
-        courses.add(new Course("Aritmetica", 20));
-        courses.add(new Course("Geometria", 50));
-        courses.add(new Course("Fisica", 60));
-        courses.add(new Course("Quimica", 80));
-        courses.add(new Course("Quimica", 70));
-        courses.add(new Course("Fisica", 30));
 
-        //stream
+        Set<String> students = new HashSet<>();
+        students.add("Luis Miguel");
+        students.add("Pepito los palotes");
+        students.add("Juan Carlos");
+        students.add("Pedro Pedrito");
+        students.add("Marcia Maria");
 
-        System.out.println(
-            courses
-                .stream()
-                .filter(course -> !course.getName().equalsIgnoreCase("Historia"))
-                .mapToInt(Course::getTime)
-                .max()
-                .getAsInt()
-        );
-
-        System.out.println(
-            courses
-                .stream()
-                .mapToInt(Course::getTime)
-                .average()
-                .getAsDouble()
-        );
-
-        System.out.println(
-            courses
-                .stream()
-                .mapToInt(Course::getTime)
-                .max()
-                .getAsInt()
-        );
-
-        System.out.println(
-            courses
-                .stream()
-                .mapToInt(Course::getTime)
-                .min()
-                .getAsInt()
-        );
-
-        Map<String, List<Course>> courseGroup = courses
-            .stream()
-            .collect(Collectors.groupingBy(Course::getName));
-
-        courseGroup.forEach((key, value) -> System.out.println(key + " : " + value.size()));
-
-        System.out.println(
-            courses
-                .parallelStream()
-                .mapToInt(Course::getTime)
-                .sum()
-        );
+        for(String student : students) {
+            System.out.println(student);
+        }
     }
 }
