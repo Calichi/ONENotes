d5e8598a8d72ce7561046497d06164a452ade6a8
Carlos Hernández <Calichi15@hotmail.com>
2023-03-08 18:54:51 -0600

    Otros métodos usando Collections y Streams

--- Dominando las collections/alura/src/alura/com/App.java
+++ Dominando las collections/alura/src/alura/com/App.java
@@ -1,6 +1,10 @@
 package alura.com;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.List;
+import java.util.Map;
+
 import alura.com.model.Course;
 
 public class App {
@@ -10,13 +14,10 @@ public class App {
         courses.add(new Course("Algebra", 10));
         courses.add(new Course("Aritmetica", 20));
         courses.add(new Course("Geometria", 50));
-
-        int time = 0;
-        for(Course course : courses) {
-            time += course.getTime();
-        }
-
-        System.out.println(time);
+        courses.add(new Course("Fisica", 60));
+        courses.add(new Course("Quimica", 80));
+        courses.add(new Course("Quimica", 70));
+        courses.add(new Course("Fisica", 30));
 
         //stream
 
@@ -27,6 +28,43 @@ public class App {
                 .mapToInt(Course::getTime)
                 .max()
                 .getAsInt()
+        );
+
+        System.out.println(
+            courses
+                .stream()
+                .mapToInt(Course::getTime)
+                .average()
+                .getAsDouble()
+        );
+
+        System.out.println(
+            courses
+                .stream()
+                .mapToInt(Course::getTime)
+                .max()
+                .getAsInt()
+        );
+
+        System.out.println(
+            courses
+                .stream()
+                .mapToInt(Course::getTime)
+                .min()
+                .getAsInt()
+        );
+
+        Map<String, List<Course>> courseGroup = courses
+            .stream()
+            .collect(Collectors.groupingBy(Course::getName));
+
+        courseGroup.forEach((key, value) -> System.out.println(key + " : " + value.size()));
+
+        System.out.println(
+            courses
+                .parallelStream()
+                .mapToInt(Course::getTime)
+                .sum()
         );
     }
 }
